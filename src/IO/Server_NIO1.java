bcb468bdbecd297ddab9c092352c5370541df3e5
qinghong.zhu <751277451@qq.com>
2020-07-29 17:16:02 +0800

    update

--- src/IO/Server_NIO1.java
+++ src/IO/Server_NIO1.java
@@ -1,5 +1,15 @@
 package IO;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -28,20 +38,103 @@ import java.util.concurrent.locks.ReentrantLock;
  * select可以传入连接文件描述符、 读文件描述符、写文件描述符
  *
  * epoll_create
- *  创建fd1
+ *  开辟内核空间
  *
  * epoll_ctl
- *  将fd1假如内核内存空间，监听一个accept
+ *  创建fd1，加入内核空间，监听一个accept
  *
  *
  * epoll_wait
  *   获取IO状态
  *
  */
+
+
+/**
+ *
+ *
+ * Reactor模式
+ *
+ */
 public class Server_NIO1 {
+    public static  void main(String[] args) throws IOException {
+        ServerSocketChannel ss = ServerSocketChannel.open();
+        ss.configureBlocking(false);
+        ss.bind(new InetSocketAddress("127.0.0.1",9090));
 
-    public static  void main(String[] args){
+        Selector selector = Selector.open();
 
+        // 注册监听事件
+        ss.register(selector, SelectionKey.OP_ACCEPT);
+
+
+        while (true){
+            Set<SelectionKey> keys = selector.keys();
+
+            System.out.println(keys.size()+ "    size");
+
+             // 阻塞方法，有事件发生了
+            while (selector.select(500)>0){
+                Set<SelectionKey> selectionKeys  = selector.selectedKeys();
+                Iterator<SelectionKey> iter = selectionKeys.iterator();
+
+                while (iter.hasNext()) {
+                    SelectionKey key = iter.next();
+                    iter.remove();
+
+                    if(key.isAcceptable()){
+
+                        ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
+                        SocketChannel client =ssc.accept();
+
+                        client.configureBlocking(false);
+
+                        ByteBuffer buffer = ByteBuffer.allocateDirect(8192);
+
+                        client.register(selector,SelectionKey.OP_READ,buffer);
+
+                        System.out.println("-------------------------------");
+
+                        System.out.println("新客户端连接" + client.getRemoteAddress());
+
+                        System.out.println("-------------------------------");
+
+
+                    }else if(key.isReadable()){
+                        SocketChannel sc = null;
+                        try {
+                            sc = (SocketChannel) key.channel();
+                            ByteBuffer buffer  = ByteBuffer.allocateDirect(512);
+                            buffer.clear();
+
+                            int len = sc.read(buffer);
+
+                            // 这个很重要哦
+                            buffer.flip();
+                            if (len!=-1){
+                                byte[] aaa = new byte[buffer.limit()];
+                                buffer.get(aaa);
+                                String b = new String(aaa);
+                                System.out.println(b);
+                            }
+
+                            ByteBuffer byteBufferTOWrite  = ByteBuffer.wrap("Welocome".getBytes());
+                            sc.write(byteBufferTOWrite);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        } finally {
+                            if(sc!=null){
+                                try {
+                                    sc.close();
+                                }catch (IOException e){
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
 
     }
